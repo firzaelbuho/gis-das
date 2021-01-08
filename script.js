95a1b110d015452841519308bc537f6115326038
firzaelbuho <firzaaditya1315@gmail.com>
2021-01-08 18:29:47 +0700

    add index

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ function initMap() {
     myMap = L.map(
         "map",
         {
-            center: [-6.902122397865429, 109.52591907491608],
+            center: [-6.89507920179603, 109.446951876763],
             crs: L.CRS.EPSG3857,
-            zoom: 14,
+            zoom: 12,
             zoomControl: true,
             preferCanvas: false,
         }
@@ -45,20 +45,36 @@ function setUpMap(data) {
             {}
         ).addTo(myMap);
 
+        let imgIcon = "img/s.png"
 
-        var custom_icon = L.icon({ "iconSize": 32, "iconUrl": "img/biru.png" });
+        if (myData.banjir == "ya" && myData.longsor == "ya") {
+
+            imgIcon = "img/sbl.png"
+
+
+        }
+
+
+        var custom_icon = L.icon({ "iconSize": 32, "iconUrl": imgIcon });
         marker.setIcon(custom_icon);
+
+        // add icon status sungai
+
+
 
 
         var popup = L.popup({ "maxWidth": "100%" });
 
         var popupHtml = `<div id="html_077d38fa84594d43bafb4e032b5c88df" 
                             style="width: 400px; height: 100.0%;">
-                            <small>30-04-2020 18:39 WIB</small>
+                    
                             <h4><b>${myData.nama}</b></h4>
-                            <img 
-                                class="py-3" width = "250px" height = "auto"
-                                src="https://lh3.googleusercontent.com/proxy/GUnQWFAjDiJ-J9_E8jd589J3mEljzo0lC59Ld4LcwUoi7zhEvJppvWdCq6gxNpMP3A49hzEr7-nZmIyLGSveGMV-1f5YDnhaA0bF4oVSPdjTFePsiHboznV9EdHfO7fhVgsnGlEx6M03QdMSV61kUg"/>
+                            <div class="my-3">
+                    <img 
+                                    width = "250px" height = "auto"
+                                    src="${myData.urlgambar}" alt="gambar"/>
+                    </div>
+                    <br><br>
                             <table cellpadding="20" width="400px" id="popupTable">
                                 <tr>
                                    <td>Panjang</td>
